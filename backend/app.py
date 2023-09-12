40a0aaf75aaa52fca2dd5ed8295e8c7f045f9ff5
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-09-12 23:57:28 +0000

    deploy

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask.helpers import send_from_directory
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_cors import CORS
 
 
 # create the extension
