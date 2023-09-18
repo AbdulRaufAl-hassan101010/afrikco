d50e930f9df95123a48cc3ba5828e4549fc2274e
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-09-18 17:55:56 +0000

    patch

--- backend/server/apis/role.py
+++ backend/server/apis/role.py
@@ -8,13 +8,13 @@ def add_role_route():
     return add_role()
 
 # Define the route for getting a role by id or getting all roles
-@apis_blueprint.route('/roles', methods=['GET'])
-@apis_blueprint.route('/roles/<int:id>', methods=['GET'])
+@apis_blueprint.route('roles', methods=['GET'])
+@apis_blueprint.route('roles/<int:id>', methods=['GET'])
 def get_roles_route(id=None):
     return get_roles(id)
 
 
 # Define the route for updating a role by id
-@apis_blueprint.route('/roles/<int:id>', methods=['PUT'])
+@apis_blueprint.route('roles/<int:id>', methods=['PUT'])
 def update_role_route(id):
     return update_role(id)
