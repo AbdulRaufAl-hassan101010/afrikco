a65073fd2bdc5606486f30a7be56bacf128d4b26
AbdulRaufAl-hassan101010 <abdulraufalhassan101010@gmail.com>
2023-10-06 00:32:53 +0000

    please work

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -59,13 +59,13 @@ app.register_blueprint(apis_blueprint, url_prefix="/apis")
 @app.route('/password-reset')
 @app.route('/password-reset/<string:token>')
 @app.route('/users/success')
-@app.route('/users/verify/<string:token>')
+@app.route('/users/verify/<string:tok>')
 @app.route('/users/verification/email')
 @app.route('/dashboard')
 @app.route('/dashboard/products/add')
 @app.route('/dashboard/orders')
 @app.route('/dashboard/users')
-def static_pages():
+def static_pages(token, tok, id):
     return send_from_directory(app.static_folder, 'index.html')
 
 
